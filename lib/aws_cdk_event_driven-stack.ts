25aa4006fc2521994d31b8bd38682cc98aa82e7d
Jorge Ramirez <jorge_ramirez@outlook.com>
2023-07-15 09:09:27 -0500

    import existing S3 bucket

--- lib/aws_cdk_event_driven-stack.ts
+++ lib/aws_cdk_event_driven-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, aws_s3 as s3 } from "aws-cdk-lib";
+import { Construct } from "constructs";
+// import { aws_s3 as s3 } from "aws-cdk-lib";
+// import * as cdk from 'aws-cdk-lib';
 
-export class AwsCdkEventDrivenStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class AwsCdkEventDrivenStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // This time we are importing an existing S3 Bucket
+    const bucketArn = s3.Bucket.fromBucketArn(
+      this,
+      "imported-bucket-from-arn",
+      "arn:aws:s3:::cdk-portfolio-files",
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkEventDrivenQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    console.log("Bucket Name => ", bucketArn.bucketName);
+    console.log("Bucket arn => ", bucketArn.bucketArn);
   }
 }
