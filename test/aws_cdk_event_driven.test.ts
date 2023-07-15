2d3f74c86dfffaa2ce71e8e16d09d17b5b4d3a39
Jorge Ramirez <jorge_ramirez@outlook.com>
2023-07-15 08:45:13 -0500

    create empty project

---
+++ test/aws_cdk_event_driven.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkEventDriven from '../lib/aws_cdk_event_driven-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_cdk_event_driven-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkEventDriven.AwsCdkEventDrivenStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
